b49fdaf663604f34b6c9dec9956c0636eb1d86cf
MESLE Alexandre <amesle@D30-01.local>
2018-09-25 10:21:31 +0200

    Suppression de la macro dans exempleTableau.c

--- tableaux/exempleTableau.c
+++ tableaux/exempleTableau.c
@@ -2,27 +2,24 @@
 
 #define N 10
 
-#define SAISIR_INT(num, nom) \
-{ \
-  printf("%d : ", num); \
-  scanf("%d", &(nom)); \
-}
-
 int main()
 {
   int E[N], i, t;
   printf("Saisissez dix valeurs : \n");
   for (i = 0 ; i < N ; i++)
-    SAISIR_INT(i+1, E[i]);
+    {
+      printf("%d : ", i+1);
+      scanf("%d", &E[i]);
+    }
   printf("Saissez une valeur \n");
   scanf("%d", &t);
   i = 0;
   while (i < N && E[i] != t)
-    i++;
-  if (i == N)
-    printf("%d ne fait pas partie des dix valeurs saisies", t);
-  else
-    printf("%d est la %d-eme valeur saisie", t, i+1);
+    {printf("%d %d\n", i, E[i]);i++;}
+    if (i == N)
+      printf("%d ne fait pas partie des dix valeurs saisies", t);
+    else
+      printf("%d est la %d-eme valeur saisie", t, i+1);
   printf("\n");
   return 0;
 }
