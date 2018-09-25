ee658eede2a863cedfc22f74e9c5d22394a43390
MESLE Alexandre <amesle@D30-01.local>
2018-09-25 09:48:01 +0200

    Phrase sur une seule ligne dans affichageChaine.c

--- chaines/affichageChaine.c
+++ chaines/affichageChaine.c
@@ -2,8 +2,7 @@
 
 int main()
 {
-  char s[64] = "Les framboises sont perchees "
-    "sur le tabouret de mon grand-pere.";
+  char s[64] = "Les framboises sont perchees sur le tabouret de mon grand-pere.";
   printf("%s\n", s);
   return 0;
 }
